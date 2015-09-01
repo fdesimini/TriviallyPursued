a5b3beb7b5c63b895c1b8205454dbc7e5d02d32f
fdesimini <fdesimini@users.noreply.github.com>
2015-09-01 14:49:37 -0400

    working with Kwame to create a for loop

---
+++ TriviallyPursued/DataModel.swift
@@ -0,0 +1,22 @@
+//
+//  DataModel.swift
+//  TriviallyPursued
+//
+//  Created by Frank Desimini on 2015-08-31.
+//  Copyright (c) 2015 Frank Desimini. All rights reserved.
+//
+
+import Foundation
+
+struct Trivia {
+    
+    var name:String?
+    var country:String?
+    var trivia:String?
+
+}
+
+
+
+
+
--- TriviallyPursued/ViewController.swift
+++ TriviallyPursued/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+        
+   // var trivia0 = Trivia(name: "CN Tower", country: "Toronto, Canada", trivia: "Largest Building")
+  //  var trivia1 = Trivia(name: "Burger Priest", country: "Toronto, Canada", trivia: "Largest Building")
+  //  var trivia2 = Trivia(name: "My House", country: "Toronto, Canada", trivia: "Largest Building")
 
+    var trivias = [Trivia]()
+    var addString = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for var i = 0 ; i < 10 ; i++ {
+            var dataString = "name\(i)"
+            var nameTrivia = Trivia(name: dataString, country: "Toronto, Canada", trivia: "Largest Building")
+            trivias.append(nameTrivia)
+            
+       // var Trivia(name: "CN Tower", country: "Toronto, Canada", trivia: "Largest Building")
+            
+        }
+        for var i = 0 ; i < trivias.count; i++ {
+            println(trivias[i].name)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
